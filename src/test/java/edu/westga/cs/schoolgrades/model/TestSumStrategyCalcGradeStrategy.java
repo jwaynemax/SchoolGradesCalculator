b2c526dba6061cd4bff9c8c1d7c403857f41dfa6
jm00724 <jm00724@csvlab-vm03.uwg.westga.edu>
2023-10-13 21:46:29 -0400

    Additional SumStrategy tests added

--- src/test/java/edu/westga/cs/schoolgrades/model/TestSumStrategyCalcGradeStrategy.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestSumStrategyCalcGradeStrategy.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class TestSumStrategyCalcGradeStrategy {
 
 	@Test
-	void testStrategyWithSmallGradesList() {
+	void testStrategyWithGradesList() {
 		Grade grade1 = new SimpleGrade(10.00);
 		Grade grade2 = new SimpleGrade(20.00);
 		Grade grade3 = new SimpleGrade(30.00);
@@ -21,6 +21,29 @@ class TestSumStrategyCalcGradeStrategy {
 		GradeStrategy strategy = new SumStrategy();
 		
 		assertEquals(60.00, comp.gradeStrategy(strategy));
+
+	}
+	
+	@Test
+	void testStrategyWithNoGrades() {
+		CompositeGrade comp = new CompositeGrade();
+		
+		GradeStrategy strategy = new SumStrategy();
+		
+		assertEquals(0.00, comp.gradeStrategy(strategy));
+	}
+	
+	@Test
+	void testStrategyWithOneGrade() {
+		Grade grade1 = new SimpleGrade(10.00);
+		
+		CompositeGrade comp = new CompositeGrade();
+		
+		comp.addGrade(grade1);
+		
+		GradeStrategy strategy = new SumStrategy();
+		
+		assertEquals(10.00, comp.gradeStrategy(strategy));
 
 	}
 
