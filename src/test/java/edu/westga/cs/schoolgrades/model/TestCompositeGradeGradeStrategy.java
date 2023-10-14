7ee0845b7920315977f14a5d7b0717bded532ca0
jm00724 <jm00724@csvlab-vm03.uwg.westga.edu>
2023-10-13 22:03:17 -0400

    Test case added to TestCompositeGradeGradeStrategy for testing AvgStratey

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeGradeStrategy.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeGradeStrategy.java
@@ -33,4 +33,21 @@ class TestCompositeGradeGradeStrategy {
 		assertEquals(240.00, comp.gradeStrategy(strategy));
 	}
 	
+	@Test
+	void testGradeStrategyWithAvg() {
+		
+		Grade grade1 = new SimpleGrade(80);
+		Grade grade2 = new SimpleGrade(90);
+		Grade grade3 = new SimpleGrade(40);
+		
+		CompositeGrade comp = new CompositeGrade();
+		comp.addGrade(grade3);
+		comp.addGrade(grade2);
+		comp.addGrade(grade1);
+		
+		GradeStrategy strategy = new AvgStrategy();
+		
+		assertEquals(70.00, comp.gradeStrategy(strategy));
+	}
+	
 }
