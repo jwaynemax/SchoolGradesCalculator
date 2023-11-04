ab71f0be18307965f4dedc09720afa117c437dbc
jm00724 <jm00724@csvlab-vm14.uwg.westga.edu>
2023-11-03 22:51:15 -0400

    Calculate subtotals for homework and exam grades

--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -3,51 +3,56 @@ package edu.westga.cs.schoolgrades.controllers;
 
 import edu.westga.cs.schoolgrades.model.AverageOfGradesStrategy;
 import edu.westga.cs.schoolgrades.model.CompositeGrade;
-import edu.westga.cs.schoolgrades.model.Grade;
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
 import edu.westga.cs.schoolgrades.model.SumOfGradesStrategy;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
-import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.util.StringConverter;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.util.converter.DoubleStringConverter;
 
-
-import java.util.Optional;
 
 public class SchoolGradesController {
-
-    @FXML
-    private ListView<Double> quizListView;
-    
+	
+	 @FXML
+    private ListView<Double> examListView;
+	
     @FXML
     private ListView<Double> hwListView;
+    
+	@FXML
+    private ListView<Double> quizListView;
+    
+	@FXML
+    private ObservableList<Double> examList = FXCollections.observableArrayList(0.00);
+	
+    @FXML
+    private ObservableList<Double> hwList = FXCollections.observableArrayList(0.00);
 
     @FXML
     private ObservableList<Double> quizList = FXCollections.observableArrayList(0.00);
     
     @FXML
-    private ObservableList<Double> hwList = FXCollections.observableArrayList(0.00);
+   	private TextField examSubtotal;
+    
+    @FXML
+   	private TextField hwSubtotal;
     
     @FXML
 	private TextField quizSubtotal;
     
-    @FXML
-	private TextField hwSubtotal;
-    
-	private DoubleProperty quizDoubleProp = new SimpleDoubleProperty();
+    private DoubleProperty examDoubleProp = new SimpleDoubleProperty();    
 	private DoubleProperty hwDoubleProp = new SimpleDoubleProperty();
+    private DoubleProperty quizDoubleProp = new SimpleDoubleProperty();
+    
+    private SimpleGrade examSimpleGrade;
+    private AverageOfGradesStrategy avgExam = new AverageOfGradesStrategy();
+    private CompositeGrade examComp = new CompositeGrade(avgExam);
 
 	private SimpleGrade hwSimpleGrade;
     private AverageOfGradesStrategy avgHw = new AverageOfGradesStrategy();
@@ -59,6 +64,10 @@ public class SchoolGradesController {
 
     
     	public void initialize() {
+    		examListView.setItems(examList);
+    		examListView.setEditable(true);
+    		examListView.setCellFactory(param -> new TextFieldListCell<>(new DoubleStringConverter()));
+    		
     		hwListView.setItems(hwList);
     		hwListView.setEditable(true);
     		hwListView.setCellFactory(param -> new TextFieldListCell<>(new DoubleStringConverter()));
@@ -79,6 +88,15 @@ public class SchoolGradesController {
     		    }
     		}
     		
+    		examListView.setOnEditCommit(event -> {
+                int index = event.getIndex();
+                Double newValue = event.getNewValue();
+                examList.set(index, newValue);
+                
+                examSimpleGrade = new SimpleGrade(newValue);
+                examComp.add(examSimpleGrade);
+            });
+    		
     		hwListView.setOnEditCommit(event -> {
                 int index = event.getIndex();
                 Double newValue = event.getNewValue();
@@ -98,6 +116,11 @@ public class SchoolGradesController {
             });
         }
 
+    	@FXML
+        public void addExamMenuItemClicked() {
+    		examList.add(0.00);
+    		
+        }
     
     	@FXML
         public void addHwMenuItemClicked() {
@@ -114,12 +137,14 @@ public class SchoolGradesController {
     	@FXML
         public void recalculate() {
     		
+    		this.examSubtotal.textProperty().bind(this.examDoubleProp.asString());
+			this.examDoubleProp.set(this.examComp.getValue());
+    		
     		this.hwSubtotal.textProperty().bind(this.hwDoubleProp.asString());
 			this.hwDoubleProp.set(this.hwComp.getValue());
     		
     		this.quizSubtotal.textProperty().bind(this.quizDoubleProp.asString());
 			this.quizDoubleProp.set(this.quizComp.getValue());
-    		    		
-    		    		    		
+    		    			    		
         }
 }
